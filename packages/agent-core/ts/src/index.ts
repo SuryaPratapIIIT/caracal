2c41a8bd30df4c5843d038c91d081936c244e7a6
RAWx18 <rawx18.dev@gmail.com>
2026-05-09 16:40:24 +0530

    chore: Remove TypeScript agent-core package and its related files

--- packages/agent-core/ts/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-// Caracal, a product of Garudex Labs
-//
-// Public surface of @caracalai/agent-core: runtime, server, types, base adapter.
-
-export * from './types.js'
-export * from './runtime.js'
-export * from './server.js'
-export * from './adapters/custom.js'
