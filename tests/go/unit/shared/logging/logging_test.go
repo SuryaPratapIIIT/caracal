1df5ddaf9a51401cd7f8f020649384bce896519e
RAWx18 <rawx18.dev@gmail.com>
2026-05-10 14:00:46 +0530

    feat: update Dockerfiles to use new transport paths; refactor logging tests and remove obsolete TypeScript test files

--- tests/go/unit/shared/logging/logging_test.go
+++ tests/go/unit/shared/logging/logging_test.go
@@ -11,23 +11,31 @@ import (
 	"github.com/rs/zerolog"
 )
 
-func TestNewSetsServiceFieldAndLogLevel(t *testing.T) {
+func TestNewAttachesServiceFieldAndPerLoggerLevel(t *testing.T) {
 	t.Setenv("LOG_LEVEL", "debug")
 	logger := New("gateway")
 
-	if zerolog.GlobalLevel() != zerolog.DebugLevel {
-		t.Fatalf("want debug global level, got %s", zerolog.GlobalLevel())
-	}
-	if logger.GetLevel() != zerolog.TraceLevel {
-		t.Fatalf("want logger level trace for inherited global filtering, got %s", logger.GetLevel())
+	if logger.GetLevel() != zerolog.DebugLevel {
+		t.Fatalf("want debug logger level, got %s", logger.GetLevel())
 	}
 }
 
 func TestNewDefaultsUnknownLevelToInfo(t *testing.T) {
 	t.Setenv("LOG_LEVEL", "verbose")
-	New("sts")
+	logger := New("sts")
 
-	if zerolog.GlobalLevel() != zerolog.InfoLevel {
-		t.Fatalf("want info global level, got %s", zerolog.GlobalLevel())
+	if logger.GetLevel() != zerolog.InfoLevel {
+		t.Fatalf("want info logger level, got %s", logger.GetLevel())
+	}
+}
+
+func TestSetGlobalLevelHonoursEnv(t *testing.T) {
+	t.Setenv("LOG_LEVEL", "warn")
+	prev := zerolog.GlobalLevel()
+	defer zerolog.SetGlobalLevel(prev)
+
+	SetGlobalLevel()
+	if zerolog.GlobalLevel() != zerolog.WarnLevel {
+		t.Fatalf("want warn global level, got %s", zerolog.GlobalLevel())
 	}
 }
